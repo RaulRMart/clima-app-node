6dd2e318495ebb0cbf90cf20b188e093d44179ca
Raul Ruperto <rrm990621@gmail.com>
2020-06-25 17:54:47 -0500

    Se modificó una linea del código

--- clima/clima.js
+++ clima/clima.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const getClima = async(lat, lng) => {
-    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=32f843d833c38373032f825c4a92418a&units=metric`)
+    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid="clave id de weather"&units=metric`)
 
     return resp.data.main.temp;
 }
